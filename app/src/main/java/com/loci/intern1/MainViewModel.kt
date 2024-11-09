48d1d52de2697bc81e0ba671bdafafc1e043b9df
kdbswo <kdbswo@gmail.com>
2024-11-09 21:56:15 +0900

    Add: viewmodel에 login 함수 추가

--- app/src/main/java/com/loci/intern1/MainViewModel.kt
+++ app/src/main/java/com/loci/intern1/MainViewModel.kt
@@ -1,13 +1,32 @@
 package com.loci.intern1
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    var auth: FirebaseAuth? = FirebaseAuth.getInstance()
+    var auth: FirebaseAuth? = null
 
+    private val _isLogin = MutableStateFlow<Boolean>(false)
+    val isLogin: StateFlow<Boolean> = _isLogin
 
+    init {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            auth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener { task ->
+                    _isLogin.value = task.isSuccessful
+                }
+        }
+    }
 
 
 }
+
