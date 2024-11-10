9b83fc9863b7c517673a225e13ef2bd949597574
kdbswo <kdbswo@gmail.com>
2024-11-10 15:00:05 +0900

    Add: Home UI 생성

--- app/src/main/java/com/loci/intern1/HomeScreen.kt
+++ app/src/main/java/com/loci/intern1/HomeScreen.kt
@@ -1,7 +1,14 @@
 package com.loci.intern1
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -9,7 +16,17 @@ import com.loci.intern1.ui.theme.Intern1Theme
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Text(text = "Home")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(text = "Home")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "로그아웃")
+        }
+    }
 }
 
 
@@ -18,7 +35,7 @@ fun HomeScreen(navController: NavHostController) {
 fun HomePreview() {
     Intern1Theme {
         val navController = rememberNavController()
-        
+
         HomeScreen(navController)
     }
 }
